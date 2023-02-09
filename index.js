75917017b40dbd2c00ab69855cb1b2f7489ac190
Andrew <64922875+Andy-Bracket@users.noreply.github.com>
2023-02-09 17:17:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = function getRandom(){
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+    // document.getElementsByClassName('img1').innerHTML = "img src = images/dice"+randomNumber1+".png";
+    // document.getElementsByClassName('img2').innerHTML += "img src = images/dice"+randomNumber2+".png";
+
+    document.querySelector('.img1').setAttribute("src", "images/dice"+randomNumber1+".png")
+    document.querySelector('.img2').setAttribute("src", "images/dice"+randomNumber2+".png")
+}

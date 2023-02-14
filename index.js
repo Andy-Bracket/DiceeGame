3871c8e4b707d29934729ceb3b30b9df794f0ef7
Loki <andrew.smit777@gmail.com>
2023-02-14 09:03:28 +0200

    Added function to stop JS from loading immediately Added emoji's for fun sakes. Made the if statement a little neater.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+function checkRefresh() {
+    if (!sessionStorage.getItem("rollDice")) {
+      sessionStorage.setItem("rollDice", "extra")
+   
+    } else(
+      getRandom()
+    )
+   
+  }
+
+
+
+
 window.onload = function getRandom(){
     let randomNumber1 = Math.floor(Math.random() * 6) + 1;
     let randomNumber2 = Math.floor(Math.random() * 6) + 1;
@@ -9,12 +22,16 @@ window.onload = function getRandom(){
     document.querySelector('.img2').setAttribute("src", "images/dice"+randomNumber2+".png")
 
     if (randomNumber1 > randomNumber2) {
-        document.querySelector('h1').innerHTML = "Player 1 Wins!"
+        document.querySelector('h1').innerHTML = "🚩 Player 1 Wins!"
     } 
     
     else if (randomNumber1 === randomNumber2) {
         document.querySelector('h1').innerHTML = "It's a Draw!"
-    } else {
-        document.querySelector('h1').innerHTML = "Player 2 Wins!"   
+    } 
+    
+    else {
+        document.querySelector('h1').innerHTML = "Player 2 Wins! 🚩"   
     };
 }
+
+document.querySelector("body").onload = checkRefresh();
